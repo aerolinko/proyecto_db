465de7d4caf6a26e30ff205a192f933ea60457c1
aerolinko <cesarst04@gmail.com>
2025-06-12 18:12:34 -0400

    a

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import SideBarTest from "@/components/SideBarTest.vue";
+
 </script>
 
 <template>
+
+  <SideBarTest/>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
---
+++ src/components/SideBarTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app permanent color="indigo darken-3">
+      <v-list dense>
+        <v-list-item
+            v-for="item in menuItems"
+            :key="item.name"
+            link
+            @click="$router.push(item.route)"
+        >
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ item.label }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'SidebarVuetify',
+  data() {
+    return {
+      menuItems: [
+        { name: 'addSale', label: 'Add Sale', route: '/add-sale' },
+        { name: 'addBuy', label: 'Add Buy', route: '/add-buy' },
+        { name: 'manageInventory', label: 'Manage Inventory', route: '/manage-inventory' },
+        { name: 'viewReports', label: 'View Reports', route: '/reports' }
+      ]
+    }
+  }
+}
+</script>
