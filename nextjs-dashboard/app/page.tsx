c97c7bdfbcba83733c72d3624c0d4c9528df8257
aerolinko <cesarst04@gmail.com>
2025-06-13 17:21:53 -0400

    cambios para añadir landing page
    adwawawd

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,53 +3,12 @@
 import { useState } from "react";
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-
-    async function handleSubmit(event: { preventDefault: () => void; }) {
-        event.preventDefault();
-        setError(""); // Clear previous errors
-
-        const response = await fetch("/api/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, password }),
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            window.location.href = `/dashboard/${data.res.usuario_id}`;
-        } else {
-            setError("Credenciales Inválidas. Intente nuevamente.");
-        }
-    }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <h1 className="text-2xl font-bold mb-4">Login</h1>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-                <input
-                    type="text"
-                    placeholder="Usuario"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    className="border p-2 rounded"
-                />
-                <input
-                    type="password"
-                    placeholder="Constraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    className="border p-2 rounded"
-                />
-                {error && <p className="text-red-500">{error}</p>}
-                <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-                    Login
-                </button>
-            </form>
+            <h1 className="text-2xl font-bold mb-4">AQUI VA A IR LA LANDING PAGE</h1>
+
+
         </div>
     );
 }
